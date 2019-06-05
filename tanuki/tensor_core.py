04f6d9f59bb1fdc2776adac9e8a90beb238c2946
infrub <infrub@gmail.com>
2019-06-05 14:58:49 +0900

    Impl Tensor.move_index

--- tanuki/tensor_core.py
+++ tanuki/tensor_core.py
@@ -6,6 +6,17 @@ import warnings
 from numpy import prod as soujou
 import textwrap
 
+
+def normalize_argument_labels(labels):
+    if isinstance(labels, list):
+        return labels
+    elif isinstance(labels, tuple):
+        return list(labels)
+    else:
+        return [labels]
+
+
+
 class Tensor:
     def __init__(self, data, labels, copy=False):
         if not copy and isinstance(data, xp.ndarray):
@@ -14,8 +25,8 @@ class Tensor:
             self.data = xp.asarray(data)
         self.labels = list(labels)
 
-    def copy(self):
-        return Tensor(self.data, self.labels, copy=True)
+    def copy(self, shallow=False):
+        return Tensor(self.data, self.labels, copy=not(shallow))
 
     def __repr__(self):
         return f"Tensor(data={self.data}, labels={self.labels})"
@@ -54,9 +65,9 @@ class Tensor:
 
     def set_labels(self, labels):
         if len(labels) != len(self.data.shape):
-            raise ValueError(f"Labels do not match shape of data. labels=={labels}, shape=={self.data.shape}")
+            raise ValueError(f"labels do not match shape of data. labels=={labels}, shape=={self.data.shape}")
         if len(labels) != len(set(labels)):
-            raise ValueError(f"Labels are not unique. labels=={labels}")
+            raise ValueError(f"labels are not unique. labels=={labels}")
         self._labels = list(labels)
 
     labels = property(get_labels, set_labels)
@@ -73,17 +84,49 @@ class Tensor:
 
 
     def replace_label(self, oldLabels, newLabels):
-        if isinstance(oldLabels, tuple):
-            oldLabels = list(oldLabels)
-        elif not isinstance(oldLabels, list):
-            oldLabels = [oldLabels]
-        if isinstance(newLabels, tuple):
-            newLabels = list(newLabels)
-        elif not isinstance(newLabels, list):
-            newLabels = [newLabels]
-
+        oldLabels = normalize_argument_labels(oldLabels)
+        newLabels = normalize_argument_labels(newLabels)
         for i, label in enumerate(self.labels):
             if label in oldLabels:
                 self.labels[i] = newLabels[oldLabels.index(label)]
 
-    
+
+    #methods for moving indices
+    #I assumed that rollaxis is better than moveaxis in terms of computing costs
+    def move_index_to_top(self, labelMove, inplace=True):
+        if inplace:
+            indexMoveFrom = self.index_of_label(labelMove)
+            self.labels.pop(indexMoveFrom)
+            self.labels.insert(0, labelMove)
+            self.data = xp.rollaxis(self.data, indexMoveFrom, 0)
+        else:
+            copySelf = self.copy(shallow=True)
+            copySelf.move_index_to_top(labelMove, inplace=True)
+            return copySelf
+
+    def move_index_to_bottom(self, labelMove, inplace=True):
+        if inplace:
+            indexMoveFrom = self.index_of_label(labelMove)
+            self.labels.pop(indexMoveFrom)
+            self.labels.append(labelMove)
+            self.data = xp.rollaxis(self.data, indexMoveFrom, self.ndim)
+        else:
+            copySelf = self.copy(shallow=True)
+            copySelf.move_index_to_bottom(labelMove, inplace=True)
+            return copySelf
+
+    def move_index_to_position(self, labelMove, position, inplace=True):
+        if inplace:
+            indexMoveFrom = self.index_of_label(labelMove)
+            if position == indexMoveFrom:
+                return
+            self.labels.pop(indexMoveFrom)
+            self.labels.insert(position, labelMove)
+            if position < indexMoveFrom:
+                self.data = xp.rollaxis(self.data, indexMoveFrom, position)
+            else:
+                self.data = xp.rollaxis(self.data, indexMoveFrom, position+1)
+        else:
+            copySelf = self.copy(shallow=True)
+            copySelf.move_index_to_position(labelMove, position, inplace=True)
+            return copySelf
