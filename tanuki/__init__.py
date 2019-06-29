8f72949952ef1f43a1e2dacf22c6c296d7eab9fe
infrub <infrub@gmail.com>
2019-06-30 04:08:00 +0900

    Move files

--- tanuki/__init__.py
+++ tanuki/__init__.py
@@ -3,6 +3,6 @@ from tanuki.utils import *
 from tanuki.tensor_core import *
 from tanuki.tensor_instant import *
 from tanuki.decomp import *
+from tanuki.autocontract import *
 import tanuki.matrices as matrices
 import tanuki.lattices as lattices
-import tanuki.autocontract as autocontract
--- tanuki/autocontract/__init__.py
+++
@@ -1,1 +0,0 @@
-from tanuki.autocontract.autocontract import *
