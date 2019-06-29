01bd370da3df3c18ab397f97bb58e0ce19fc374b
infrub <infrub@gmail.com>
2019-06-30 04:00:26 +0900

    Remove trashes in test

--- utests/test_autocontract.py
+++ utests/test_autocontract.py
@@ -21,7 +21,7 @@ class TestAutoContract(unittest.TestCase):
         self.assertEqual(eternity.cost, 2745)
         ABC = actor.exec()
         self.assertEqual(ABC, A*(B*C))
-    """
+    
     def test_input3(self):
         rho = TensorFrame((10,10,10,10),["i","j","k","l"])
         o1 = TensorFrame((10,10,10),["i","f1","f2"])
@@ -41,55 +41,8 @@ class TestAutoContract(unittest.TestCase):
         print(eternity)
 
         self.assertEqual(eternity.cost, 115300100)
-    """
+    
 
-
-"""
-class TestBrute(unittest.TestCase):
-    def test_brute(self):
-        A = TensorFrame((5,5),["i","j"], 0)
-        B = TensorFrame((5,7,8),["j","k","l"], 1)
-        C = TensorFrame((7,8,9),["k","l","m"], 2)
-        hog = NetconBrute([A,B,C])
-        res = hog.generate_eternity()
-        
-        self.assertEqual(res.cost, 2745)
-        f = hog.generate_contractor()
-        self.assertEqual(f(3,5,7), 105)
-
-class TestJermyn(unittest.TestCase):
-    def test_jermyn_input1(self):
-        A = TensorFrame((5,5),["i","j"])
-        B = TensorFrame((5,7,8),["j","k","l"])
-        C = TensorFrame((7,8,9),["k","l","m"])
-        hog = NetconJermyn([A,B,C])
-        res = hog.generate_eternity()
-        print(res)
-        
-        self.assertEqual(res.cost, 2745)
-        f = hog.generate_contractor()
-        self.assertEqual(f(3,5,7), 105)
-
-    def test_jermyn_input3(self):
-        rho = TensorFrame((10,10,10,10),["i","j","k","l"])
-        o1 = TensorFrame((10,10,10),["i","f1","f2"])
-        o2 = TensorFrame((10,10,10),["j","f3","f4"])
-        o3 = TensorFrame((10,10,10),["k","f5","f6"])
-        o4 = TensorFrame((10,10,10),["l","f7","f8"])
-        u1 = TensorFrame((10,10,10,10),["f1","f6","g1","g2"])
-        u2 = TensorFrame((10,10,10,10),["f5","f2","g3","g4"])
-        u3 = TensorFrame((10,10,10,10),["f3","f8","g5","g6"])
-        u4 = TensorFrame((10,10,10,10),["f7","f4","g3","g7"])
-        op1 = TensorFrame((10,10),["g2","g6"])
-        op2 = TensorFrame((10,10),["g1","g5"])
-        op3 = TensorFrame((10,10),["g4","g7"])
-        Ts = [rho,o1,o2,o3,o4,u1,u2,u3,u4,op1,op2,op3]
-        hog = NetconJermyn(Ts)
-        res = hog.generate_eternity()
-        print(res)
-
-        self.assertEqual(res.cost, 115300100)
-"""
 
 if __name__=="__main__":
     unittest.main()
