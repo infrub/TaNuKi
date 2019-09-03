0e222729cbd2ff7afe1529c1feeea0bff6fd27b8
infrub <infrub@gmail.com>
2019-09-03 21:33:13 +0900

    algname de matometa

--- experiments/day02/epm023.py
+++ experiments/day02/epm023.py
@@ -21,12 +21,12 @@ def epm0230():
     ENV = bondenv.UnbridgeBondEnv(V, ["kl"], ["kr"])
 
     memo = {}
-    M,S,N = ENV.optimal_truncate_alg01(A, maxiter=100, chi=chi, memo=memo)
+    M,S,N = ENV.optimal_truncate(A, maxiter=100, chi=chi, memo=memo, algname="alg01")
     print(memo)
     print(M*S*N)
 
     memo = {}
-    M,S,N = ENV.optimal_truncate_alg02(A, maxiter=10, chi=chi, memo=memo)
+    M,S,N = ENV.optimal_truncate(A, maxiter=10, chi=chi, memo=memo, algname="alg02")
     print(memo)
     print(M*S*N)
 
@@ -43,12 +43,12 @@ def epm0231():
     ENV = bondenv.UnbridgeBondEnv(V, ["kl"], ["kr"])
 
     memo = {}
-    M,S,N = ENV.optimal_truncate_alg01(A, maxiter=1000, chi=chi, memo=memo)
+    M,S,N = ENV.optimal_truncate(A, maxiter=1000, chi=chi, memo=memo, algname="alg01")
     print(memo)
     print(M*S*N)
 
     memo = {}
-    M,S,N = ENV.optimal_truncate_alg03(A, maxiter=1000, chi=chi, memo=memo)
+    M,S,N = ENV.optimal_truncate(A, maxiter=1000, chi=chi, memo=memo, algname="alg03")
     print(memo)
     print(M*S*N)
 
