3b57a26e10c2924dcea4893a46e936279bd844a8
infrub <infrub@gmail.com>
2019-09-03 20:22:04 +0900

    _alg03: toiuwakede saiteki no hazudaga u-n osoi..

--- experiments/day02/epm023.py
+++ experiments/day02/epm023.py
@@ -26,10 +26,31 @@ def epm0230():
     print(M*S*N)
 
     memo = {}
-    M,S,N = ENV.optimal_truncate_alg02(A, maxiter=1000, chi=chi, memo=memo)
+    M,S,N = ENV.optimal_truncate_alg02(A, maxiter=10, chi=chi, memo=memo)
     print(memo)
     print(M*S*N)
 
 
 
-test0230()
+# test katamuki keisan!
+def epm0231():
+    b = 4
+    n = b*b
+    chi = b-1
+    H = random_tensor((b,b,n),["kl","kr","extraction"])
+    V = H * H.adjoint(["kl","kr"],style="aster")
+    A = random_tensor((b,b),["kl","kr"])
+    ENV = bondenv.UnbridgeBondEnv(V, ["kl"], ["kr"])
+
+    memo = {}
+    M,S,N = ENV.optimal_truncate_alg01(A, maxiter=1000, chi=chi, memo=memo)
+    print(memo)
+    print(M*S*N)
+
+    memo = {}
+    M,S,N = ENV.optimal_truncate_alg03(A, maxiter=1000, chi=chi, memo=memo)
+    print(memo)
+    print(M*S*N)
+
+
+epm0231()
--- tanuki/bondenv/bondenv.py
+++ tanuki/bondenv/bondenv.py
@@ -158,6 +158,9 @@ class UnbridgeBondEnv:
 
 
 
+
+
+
     def optimal_truncate_alg02(self, sigma0, chi=20, maxiter=1000, conv_atol=1e-10, conv_rtol=1e-10, memo=None):
         if memo is None:
             memo = {}
@@ -264,8 +267,6 @@ class UnbridgeBondEnv:
                             for l in range(2):
                                 css[i+j][k+l] += (Tss[i][k] * ETA * Thss[j][l]).real().to_scalar()
 
-                x,y = solve_argmin_xxyy_equation(css)
-
                 print(css) # aruteido ikuto taishou gyouretu ni naru! uresii!
 
                 return css
@@ -292,6 +293,157 @@ class UnbridgeBondEnv:
 
 
         return M,S,N
+
+
+
+
+
+
+    def optimal_truncate_alg03(self, sigma0, chi=20, maxiter=1000, conv_atol=1e-10, conv_rtol=1e-10, memo=None):
+        if memo is None:
+            memo = {}
+        start_time = time.time()
+
+        chi = max(1,chi)
+        b = soujou(sigma0.dims(self.ket_left_labels))
+        memo["b"] = b
+        n = numpy.linalg.matrix_rank(self.tensor.to_matrix(self.ket_left_labels+self.ket_right_labels), hermitian=True)
+        memo["n"] = n
+        max_chi_can_use_iterating_method = n // b # = floor(n/b)
+        min_chi_can_use_exact_solving = (n-1)//b+1 # = ceil(n/b)
+        exactly_solvable_chi = math.ceil(b - math.sqrt(b**2-n))
+
+        if chi >= min_chi_can_use_exact_solving:
+            chi = min_chi_can_use_exact_solving
+            #assert memo["has_enough_degree_of_freedom_to_solve_exactly"] #proven
+            memo["used_algorithm"] = "exact_solving"
+        else:
+            #assert chi <= max_chi_can_use_iterating_method #proven
+            memo["used_algorithm"] = "iterating_method"
+        memo["chi"] = chi
+        memo["has_enough_degree_of_freedom_to_solve_exactly"] = (4*b*chi - 2*chi*chi) >= 2*n
+
+
+        ket_mn_label = unique_label()
+        bra_mn_label = aster_label(ket_mn_label)
+
+        ETA = self.tensor
+        Cbase = ETA * sigma0
+
+        M,S,N = tnd.truncated_svd(sigma0, self.ket_left_labels, self.ket_right_labels, chi=chi, svd_labels = ket_mn_label)
+        M = M * S.sqrt()
+        N = S.sqrt() * N
+        del S
+
+
+        if memo["used_algorithm"] == "exact_solving":
+            # solve exactly
+            # this case includes max_chi_can_use_iterating_method==0 case. (Because if n<b: exactly_solvable_chi==1).
+
+            extraction_label = unique_label()
+            HTA,HTAw,_ = tnd.truncated_eigh(ETA, self.ket_left_labels+self.ket_right_labels, chi=chi*b, atol=0, rtol=0, eigh_labels=extraction_label) #TODO sometimes segmentation fault occurs (why?)
+            #print(HTAw)
+            Mshape = M.shape
+            B = N * HTA
+            B = B.to_matrix(extraction_label, self.ket_left_labels+[ket_mn_label])
+            C = sigma0 * HTA
+            C = C.to_vector(extraction_label)
+            M = xp.linalg.solve(B,C)
+            M = tnc.vector_to_tensor(M, Mshape, self.ket_left_labels+[ket_mn_label])
+            #print(M*N*HTA - sigma0*HTA)
+
+            memo["iter_times"] = 0
+
+
+        else: # chi < n/b  ==>  chi*b<n  ==>  B is not singular
+            assert chi <= max_chi_can_use_iterating_method # proven in test0167
+            memo["used_algorithm"] = "iterating_method"
+
+            def optimize_M_from_N(N):
+                Nh = N.adjoint(ket_mn_label, self.ket_right_labels, style="aster")
+                B = N * ETA * Nh
+                C = Cbase * Nh
+                Mshape = B.dims(self.ket_left_labels+[ket_mn_label])
+                B = B.to_matrix(self.bra_left_labels+[bra_mn_label], self.ket_left_labels+[ket_mn_label])
+                C = C.to_vector(self.bra_left_labels+[bra_mn_label])
+                M = xp.linalg.solve(B, C, assume_a="pos")
+
+                M = tnc.vector_to_tensor(M, Mshape, self.ket_left_labels+[ket_mn_label])
+                return M
+
+            def optimize_N_from_M(M):
+                Mh = M.adjoint(self.ket_left_labels, ket_mn_label, style="aster")
+                B = Mh * ETA * M
+                C = Mh * Cbase
+                Nshape = B.dims([ket_mn_label]+self.ket_right_labels)
+                B = B.to_matrix([bra_mn_label]+self.bra_right_labels, [ket_mn_label]+self.ket_right_labels)
+                C = C.to_vector([bra_mn_label]+self.bra_right_labels)
+                N = xp.linalg.solve(B, C, assume_a="pos")
+                N = tnc.vector_to_tensor(N, Nshape, [ket_mn_label]+self.ket_right_labels)
+                return N
+
+            def solve_argmin_xxxx_equation(cs):
+                # x = argmin( \sum_{i=0..4} x^i cs[i] ) # css is real, x is real.
+                xkouhos = numpy.roots([4*cs[4], 3*cs[3], 2*cs[2], 1*cs[1]])
+                x = 0
+                fx = cs[0]
+                for xkouho in xkouhos: #TODO totsu nanjanaika? dattara for iranaiyone
+                    if np.iscomplex(xkouho): continue
+                    fxkouho = cs[0] + cs[1] * xkouho + cs[2] * xkouho**2 + cs[3] * xkouho**3 + cs[4] * xkouho**4
+                    if fxkouho < fx:
+                        fx = fxkouho
+                        x = xkouho
+                return x
+
+            def get_equation_coeffs(M,N,dM,dN):
+                Tss = [[0,0],[0,0]]
+                Tss[0][0] = M*N-sigma0
+                Tss[1][0] = dM * N
+                Tss[0][1] = M * dN
+                Tss[1][1] = dM * dN
+                Thss = [[0,0],[0,0]]
+                for i in range(2):
+                    for j in range(2):
+                        Thss[i][j] = Tss[i][j].adjoint()
+                # minimize_x,y || \sum_{i=0,1}{j=0,1} x^i y^j T[i,j] * HETA ||^2
+
+                cs = [0,0,0,0,0]
+                for i in range(2):
+                    for j in range(2):
+                        for k in range(2):
+                            for l in range(2):
+                                cs[i+j+k+l] += (Tss[i][k] * ETA * Thss[j][l]).real().to_scalar()
+
+                return cs
+
+
+            for iteri in range(maxiter):
+                kariNewM = optimize_M_from_N(N)
+                kariNewN = optimize_N_from_M(M)
+                if M.__eq__(kariNewM, atol=conv_atol, rtol=conv_rtol):
+                    break
+                dM = kariNewM - M
+                dN = kariNewN - N
+                x = solve_argmin_xxxx_equation(get_equation_coeffs(M,N,dM,dN))
+                if x==0:
+                    break
+                #print(x)
+                M = M + x*dM
+                N = N + x*dN
+                #print(dM)
+            
+            memo["iter_times"] = iteri
+
+
+        M,S,N = tnd.truncated_svd(M*N, self.ket_left_labels, self.ket_right_labels, chi=chi)
+
+        memo["elapsed_time"] = time.time()-start_time
+
+
+        return M,S,N
+
+
+
 
 
 
