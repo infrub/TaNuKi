ee1e2bec1a2e2c2cd61625a292355b57aa4b05d0
infrub <infrub@gmail.com>
2019-09-04 19:32:04 +0900

    epm0238: hiromeni mitemita

--- experiments/day02/epm023.py
+++ experiments/day02/epm023.py
@@ -11,6 +11,9 @@ from matplotlib import pyplot as plt
 import matplotlib.cm as cm
 import pandas as pd
 
+pd.options.display.max_columns = 20
+pd.options.display.width = 160
+MAX_PDF_PAGE = 100
 
 # test koukahou katamukikeisannnashi
 def epm0230():
@@ -333,12 +336,153 @@ def epm0236():
         plt.xlabel("err")
         plt.legend()
         plt.title(f"epm0236[b={b},chi={chi},seed={seed}]")
-        plt.savefig(f"epm0236_oups/[b={b}, chi={chi}, seed={seed}].png", dpi=600)
+        plt.savefig(f"epm0236_oups/[b={b}, chi={chi}, seed={seed}].png", dpi=400)
 
     for seed in range(100):
         ikuze(seed)
 
 
 
+# ippai mitemiyo-
+def epm0237():
+    def ikuze(seed):
+        np.random.seed(seed=seed)
 
-epm0236()
+        b,chi = 10,8
+        n = b*b
+        H = random_tensor((b,b,n),["kl","kr","extraction"])
+        V = H * H.adjoint(["kl","kr"],style="aster")
+        A = random_tensor((b,b),["kl","kr"])
+        ENV = bondenv.UnbridgeBondEnv(V, ["kl"], ["kr"])
+
+        fig, axs = plt.subplots(1, 2, sharey=True, figsize=(13,8))
+        ax1,ax2 = tuple(axs)
+        ax1.set_xscale("log")
+        ax1.set_ylabel("precision scale")
+        ax1.set_xlabel("iteration times")
+        ax2.set_xlabel("times spent for getting over the precision range (lefter is better)")
+
+        memo = {}
+        M,S,N = ENV.optimal_truncate(A, maxiter=1000, conv_atol=1e-14, conv_rtol=1e-14, chi=chi, memo=memo, algname="alg04")
+        trueError = memo["sq_diff"]*(1-1e-10)
+
+        def yaru(algname, kasoku, color):
+            if type(kasoku) == str:
+                title = f"{algname}({kasoku})"
+            else:
+                title = f"{algname}({kasoku:4.2f})"
+            print(title)
+            memo = {}
+            M,S,N = ENV.optimal_truncate(A, maxiter=400, chi=chi, memo=memo, algname=algname, kasoku=kasoku)
+            df = pd.DataFrame({"y": memo.pop("fxs"), "x":np.arange(1,memo["iter_times"]+1)})
+            df["my"] = df.y - trueError
+            df["logx"] = np.log(df.x)
+            df["logmy"] = np.log(df.my)
+            df["dlogx"] = df.logx.shift(-1) - df.logx
+            df["dlogmy"] = df.logmy.shift(-1) - df.logmy
+            #df["-dlogx__dlogmy"] = -df["dlogx"]/df["dlogmy"] #== logmy wo herasunoni kakaru logx. tiisai houga ii!
+            #df["tadasing_cost"] = df["-dlogx__dlogmy"]
+            #df["smoothed_tadasing_cost"] = df.tadasing_cost.rolling(5).mean().shift(2)
+            df["-dx__dlogmy"] = -1.0/df["dlogmy"] #== logmy wo herasunoni kakaru x. tiisai houga ii!
+            df["tadasing_cost2"] = df["-dx__dlogmy"]
+            df["smoothed_tadasing_cost2"] = df.tadasing_cost2.rolling(5, win_type="triang").mean().shift(-2)
+
+
+            ax1.plot(df.x, df.logmy, label=title, color=color)
+            ax2.plot(df.smoothed_tadasing_cost2, df.logmy, label=title, color=color) #migikara hidarini jikanha susumuyo
+            print(S)
+            print(memo)
+            print()
+
+        yaru("alg04", "", "black")
+
+        for kasoku in np.linspace(1.65,1.95,11):
+            yaru("alg08", kasoku, cm.gist_rainbow(float(kasoku-1.65)*3.3))
+
+        yaru("alg01", 1, "gray")
+        
+        ax2.set_xlim(0,min(100,ax2.get_xlim()[1]))
+        plt.legend()
+        plt.suptitle(f"epm0237[b={b},chi={chi},seed={seed}]")
+        plt.savefig(f"epm0237_oups/[b={b}, chi={chi}, seed={seed}].png", dpi=400)
+        #plt.show()
+
+    for seed in range(100):
+        ikuze(seed)
+
+
+
+# ippai mitemiyo-
+def epm0238():
+    def ikuze(seed):
+        np.random.seed(seed=seed)
+
+        b,chi = 10,8
+        n = b*b
+        H = random_tensor((b,b,n),["kl","kr","extraction"])
+        V = H * H.adjoint(["kl","kr"],style="aster")
+        A = random_tensor((b,b),["kl","kr"])
+        ENV = bondenv.UnbridgeBondEnv(V, ["kl"], ["kr"])
+
+        fig, axs = plt.subplots(1, 2, sharey=True, figsize=(13,8))
+        ax1,ax2 = tuple(axs)
+        ax1.set_xscale("log")
+        ax1.set_ylabel("precision scale")
+        ax1.set_xlabel("iteration times")
+        ax2.set_xlabel("times spent for getting over the precision range (lefter is better)")
+
+        memo = {}
+        M,S,N = ENV.optimal_truncate(A, maxiter=1000, conv_atol=1e-14, conv_rtol=1e-14, chi=chi, memo=memo, algname="alg04")
+        trueError = memo["sq_diff"]*(1-1e-10)
+
+        def yaru(algname, kasoku, color):
+            if type(kasoku) == str:
+                title = f"{algname}({kasoku})"
+            else:
+                title = f"{algname}({kasoku:4.2f})"
+            print(title)
+            memo = {}
+            M,S,N = ENV.optimal_truncate(A, maxiter=400, chi=chi, memo=memo, algname=algname, kasoku=kasoku)
+            df = pd.DataFrame({"y": memo.pop("fxs"), "x":np.arange(1,memo["iter_times"]+1)})
+            df["my"] = df.y - trueError
+            df["logx"] = np.log(df.x)
+            df["logmy"] = np.log(df.my)
+            df["dlogx"] = df.logx.shift(-1) - df.logx
+            df["dlogmy"] = df.logmy.shift(-1) - df.logmy
+            #df["-dlogx__dlogmy"] = -df["dlogx"]/df["dlogmy"] #== logmy wo herasunoni kakaru logx. tiisai houga ii!
+            #df["tadasing_cost"] = df["-dlogx__dlogmy"]
+            #df["smoothed_tadasing_cost"] = df.tadasing_cost.rolling(5).mean().shift(2)
+            df["-dx__dlogmy"] = -1.0/df["dlogmy"] #== logmy wo herasunoni kakaru x. tiisai houga ii!
+            df["tadasing_cost2"] = df["-dx__dlogmy"]
+            df["smoothed_tadasing_cost2"] = df.tadasing_cost2.rolling(5, win_type="triang").mean().shift(-2)
+
+
+            ax1.plot(df.x, df.logmy, label=title, color=color)
+            ax2.plot(df.smoothed_tadasing_cost2, df.logmy, label=title, color=color) #migikara hidarini jikanha susumuyo
+            #print(S)
+            print(memo)
+            #print()
+
+        yaru("alg04", "", "black")
+
+        kasokus = [1.0,1.2,1.35,1.5,1.6,1.65,1.7,1.75,1.8,1.85,1.9,1.95,1.97] # 1.98 ha sasugani LinAlgWarning detekurusi, daitai ahomitai ni osoi
+        for i,kasoku in enumerate(kasokus):
+            try:
+                yaru("alg08", kasoku, cm.gist_rainbow(float(i/len(kasokus))))
+            except Exception as e:
+                print(e)
+                continue
+        
+        ax2.set_xlim(0,min(100,ax2.get_xlim()[1]))
+        plt.legend()
+        plt.suptitle(f"epm0238[b={b},chi={chi},seed={seed}]")
+        plt.savefig(f"epm0238_oups/[b={b}, chi={chi}, seed={seed}].png", dpi=400)
+        #plt.show()
+
+    for seed in range(100):
+        print(f"seed={seed}")
+        ikuze(seed)
+        print("\n")
+
+
+epm0238()
